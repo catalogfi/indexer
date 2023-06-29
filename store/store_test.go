0d065e9e1cc6bf902fca17541f98062113b953bc
IshanChhangani <f20200230@hyderabad.bits-pilani.ac.in>
2023-06-29 16:00:48 +0530

    code structured.

--- store/store_test.go
+++ store/store_test.go
@@ -20,48 +20,48 @@ func TestMyPackage(t *testing.T) {
 	RunSpecs(t, "MyPackage Suite")
 }
 
-// var _ = Describe("GetHeaderFromHeight", func() {
-// 	It("should get the blocks header given the height", func() {
-// 		db, err := model.NewDB(sqlite.Open("gorm.db"), &gorm.Config{})
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		s := store.NewStorage(&chaincfg.RegressionNetParams, db)
-// 		h, err := s.GetLatestBlockHeight()
-// 		Expect(err).To(BeNil())
-// 		prevblock := &model.Block{}
-// 		resp := db.First(prevblock, "height = ?", h)
-// 		Expect(resp.Error).To(BeNil())
-// 		prevBlockHash, err := chainhash.NewHashFromStr(prevblock.Hash)
-// 		Expect(err).To(BeNil())
+var _ = Describe("GetHeaderFromHeight", func() {
+	It("should get the blocks header given the height", func() {
+		db, err := model.NewDB(sqlite.Open("gorm.db"), &gorm.Config{})
+		if err != nil {
+			panic(err)
+		}
+		s := store.NewStorage(&chaincfg.RegressionNetParams, db)
+		h, err := s.GetLatestBlockHeight()
+		Expect(err).To(BeNil())
+		prevblock := &model.Block{}
+		resp := db.First(prevblock, "height = ?", h)
+		Expect(resp.Error).To(BeNil())
+		prevBlockHash, err := chainhash.NewHashFromStr(prevblock.Hash)
+		Expect(err).To(BeNil())
 
-// 		merkleRootHash, err := chainhash.NewHashFromStr("ae1f680b35dae02d3eadba15b0644740c09263972589a0953c1af4665d1a681a")
-// 		Expect(err).To(BeNil())
-// 		block := &wire.MsgBlock{
-// 			Header: wire.BlockHeader{
-// 				Version:    1,
-// 				PrevBlock:  *prevBlockHash,
-// 				MerkleRoot: *merkleRootHash,
-// 				Timestamp:  time.Unix(1231006505, 0), // Set a specific timestamp
-// 				Bits:       486604799,
-// 				Nonce:      2083236893,
-// 			},
-// 			Transactions: []*wire.MsgTx{},
-// 		}
+		merkleRootHash, err := chainhash.NewHashFromStr("ae1f680b35dae02d3eadba15b0644740c09263972589a0953c1af4665d1a681a")
+		Expect(err).To(BeNil())
+		block := &wire.MsgBlock{
+			Header: wire.BlockHeader{
+				Version:    1,
+				PrevBlock:  *prevBlockHash,
+				MerkleRoot: *merkleRootHash,
+				Timestamp:  time.Unix(1231006505, 0), // Set a specific timestamp
+				Bits:       486604799,
+				Nonce:      2083236893,
+			},
+			Transactions: []*wire.MsgTx{},
+		}
 
-// 		s.PutBlock(block)
+		s.PutBlock(block)
 
-// 		header, err := s.GetHeaderFromHeight(h + 1)
-// 		Expect(err).To(BeNil())
-// 		// Expect(header).To(Equal(block.Header))
-// 		Expect(header.Header.Version).To(Equal(block.Header.Version))
-// 		Expect(header.Header.PrevBlock).To(Equal(block.Header.PrevBlock))
-// 		Expect(header.Header.MerkleRoot).To(Equal(block.Header.MerkleRoot))
-// 		Expect(header.Header.Bits).To(Equal(block.Header.Bits))
-// 		Expect(header.Header.Nonce).To(Equal(block.Header.Nonce))
-// 		Expect(header.Height).To(Equal(h + 1))
-// 	})
-// })
+		header, err := s.GetHeaderFromHeight(h + 1)
+		Expect(err).To(BeNil())
+		// Expect(header).To(Equal(block.Header))
+		Expect(header.Header.Version).To(Equal(block.Header.Version))
+		Expect(header.Header.PrevBlock).To(Equal(block.Header.PrevBlock))
+		Expect(header.Header.MerkleRoot).To(Equal(block.Header.MerkleRoot))
+		Expect(header.Header.Bits).To(Equal(block.Header.Bits))
+		Expect(header.Header.Nonce).To(Equal(block.Header.Nonce))
+		Expect(header.Height).To(Equal(h + 1))
+	})
+})
 
 func GetBlockByHeight(height int32, db *gorm.DB) (*model.Block, error) {
 	block := &model.Block{}
@@ -69,167 +69,167 @@ func GetBlockByHeight(height int32, db *gorm.DB) (*model.Block, error) {
 	return block, resp.Error
 }
 
-// var _ = Describe("GetHeaderFromHash", func() {
-// 	It("should get the blocks header given the hash", func() {
-// 		db, err := model.NewDB(sqlite.Open("gorm.db"), &gorm.Config{})
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		s := store.NewStorage(&chaincfg.RegressionNetParams, db)
-// 		h, err := s.GetLatestBlockHeight()
-// 		Expect(err).To(BeNil())
-// 		prevblock := &model.Block{}
-// 		resp := db.First(prevblock, "height = ?", h)
-// 		Expect(resp.Error).To(BeNil())
-// 		prevBlockHash, err := chainhash.NewHashFromStr(prevblock.Hash)
-// 		Expect(err).To(BeNil())
+var _ = Describe("GetHeaderFromHash", func() {
+	It("should get the blocks header given the hash", func() {
+		db, err := model.NewDB(sqlite.Open("gorm.db"), &gorm.Config{})
+		if err != nil {
+			panic(err)
+		}
+		s := store.NewStorage(&chaincfg.RegressionNetParams, db)
+		h, err := s.GetLatestBlockHeight()
+		Expect(err).To(BeNil())
+		prevblock := &model.Block{}
+		resp := db.First(prevblock, "height = ?", h)
+		Expect(resp.Error).To(BeNil())
+		prevBlockHash, err := chainhash.NewHashFromStr(prevblock.Hash)
+		Expect(err).To(BeNil())
 
-// 		merkleRootHash, err := chainhash.NewHashFromStr("ad1f680b35dae02d3eadba15b0644740c09263972589a0953c1af4665d1a681a")
-// 		Expect(err).To(BeNil())
-// 		block := &wire.MsgBlock{
-// 			Header: wire.BlockHeader{
-// 				Version:    1,
-// 				PrevBlock:  *prevBlockHash,
-// 				MerkleRoot: *merkleRootHash,
-// 				Timestamp:  time.Unix(1231006505, 0), // Set a specific timestamp
-// 				Bits:       486604799,
-// 				Nonce:      2083236893,
-// 			},
-// 			Transactions: []*wire.MsgTx{},
-// 		}
+		merkleRootHash, err := chainhash.NewHashFromStr("ad1f680b35dae02d3eadba15b0644740c09263972589a0953c1af4665d1a681a")
+		Expect(err).To(BeNil())
+		block := &wire.MsgBlock{
+			Header: wire.BlockHeader{
+				Version:    1,
+				PrevBlock:  *prevBlockHash,
+				MerkleRoot: *merkleRootHash,
+				Timestamp:  time.Unix(1231006505, 0), // Set a specific timestamp
+				Bits:       486604799,
+				Nonce:      2083236893,
+			},
+			Transactions: []*wire.MsgTx{},
+		}
 
-// 		s.PutBlock(block)
+		s.PutBlock(block)
 
-// 		// Expect(db.Create(block).Error).To(BeNil())
-// 		header, err := s.GetHeaderFromHash(block.Header.BlockHash().String())
-// 		Expect(err).To(BeNil())
-// 		Expect(header.Header.Version).To(Equal(block.Header.Version))
-// 		Expect(header.Header.PrevBlock).To(Equal(block.Header.PrevBlock))
-// 		Expect(header.Header.MerkleRoot).To(Equal(block.Header.MerkleRoot))
-// 		Expect(header.Header.Bits).To(Equal(block.Header.Bits))
-// 		Expect(header.Header.Nonce).To(Equal(block.Header.Nonce))
-// 		Expect(header.Height).To(Equal(h + 1))
-// 		// Expect(header.MsgBlock().Header.Version).To(Equal(block.Header.Version))
-// 	})
-// })
+		// Expect(db.Create(block).Error).To(BeNil())
+		header, err := s.GetHeaderFromHash(block.Header.BlockHash().String())
+		Expect(err).To(BeNil())
+		Expect(header.Header.Version).To(Equal(block.Header.Version))
+		Expect(header.Header.PrevBlock).To(Equal(block.Header.PrevBlock))
+		Expect(header.Header.MerkleRoot).To(Equal(block.Header.MerkleRoot))
+		Expect(header.Header.Bits).To(Equal(block.Header.Bits))
+		Expect(header.Header.Nonce).To(Equal(block.Header.Nonce))
+		Expect(header.Height).To(Equal(h + 1))
+		// Expect(header.MsgBlock().Header.Version).To(Equal(block.Header.Version))
+	})
+})
 
-// var _ = Describe("GetBlockFromHash", func() {
-// 	It("should get the blocks given the hash", func() {
-// 		db, err := model.NewDB(sqlite.Open("gorm.db"), &gorm.Config{})
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		s := store.NewStorage(&chaincfg.RegressionNetParams, db)
-// 		h, err := s.GetLatestBlockHeight()
-// 		Expect(err).To(BeNil())
-// 		prevblock := &model.Block{}
-// 		resp := db.First(prevblock, "height = ?", h)
-// 		Expect(resp.Error).To(BeNil())
-// 		prevBlockHash, err := chainhash.NewHashFromStr(prevblock.Hash)
-// 		Expect(err).To(BeNil())
+var _ = Describe("GetBlockFromHash", func() {
+	It("should get the blocks given the hash", func() {
+		db, err := model.NewDB(sqlite.Open("gorm.db"), &gorm.Config{})
+		if err != nil {
+			panic(err)
+		}
+		s := store.NewStorage(&chaincfg.RegressionNetParams, db)
+		h, err := s.GetLatestBlockHeight()
+		Expect(err).To(BeNil())
+		prevblock := &model.Block{}
+		resp := db.First(prevblock, "height = ?", h)
+		Expect(resp.Error).To(BeNil())
+		prevBlockHash, err := chainhash.NewHashFromStr(prevblock.Hash)
+		Expect(err).To(BeNil())
 
-// 		merkleRootHash, err := chainhash.NewHashFromStr("ad1f680b35dae02d3eadba15b0644740c09263972589a0953c1af4665d1a681a")
-// 		Expect(err).To(BeNil())
-// 		block := &wire.MsgBlock{
-// 			Header: wire.BlockHeader{
-// 				Version:    1,
-// 				PrevBlock:  *prevBlockHash,
-// 				MerkleRoot: *merkleRootHash,
-// 				Timestamp:  time.Unix(1231006505, 0), // Set a specific timestamp
-// 				Bits:       486604799,
-// 				Nonce:      2083236893,
-// 			},
-// 			Transactions: []*wire.MsgTx{},
-// 		}
+		merkleRootHash, err := chainhash.NewHashFromStr("ad1f680b35dae02d3eadba15b0644740c09263972589a0953c1af4665d1a681a")
+		Expect(err).To(BeNil())
+		block := &wire.MsgBlock{
+			Header: wire.BlockHeader{
+				Version:    1,
+				PrevBlock:  *prevBlockHash,
+				MerkleRoot: *merkleRootHash,
+				Timestamp:  time.Unix(1231006505, 0), // Set a specific timestamp
+				Bits:       486604799,
+				Nonce:      2083236893,
+			},
+			Transactions: []*wire.MsgTx{},
+		}
 
-// 		s.PutBlock(block)
+		s.PutBlock(block)
 
-// 		// Expect(db.Create(block).Error).To(BeNil())
-// 		btcblock1, err := s.GetBlockFromHash(block.Header.BlockHash().String())
-// 		Expect(err).To(BeNil())
-// 		Expect(btcblock1.MsgBlock().Header.Version).To(Equal(block.Header.Version))
-// 		Expect(btcblock1.MsgBlock().Header.MerkleRoot).To(Equal(block.Header.MerkleRoot))
-// 		Expect(btcblock1.MsgBlock().Header.Bits).To(Equal(block.Header.Bits))
-// 		Expect(btcblock1.MsgBlock().Header.Nonce).To(Equal(block.Header.Nonce))
-// 		// Expect(btcblock1.MsgBlock().Header.Timestamp).To(Equal(block.Header.Timestamp))
-// 		Expect(btcblock1.MsgBlock().Header.PrevBlock).To(Equal(block.Header.PrevBlock))
-// 	})
-// })
+		// Expect(db.Create(block).Error).To(BeNil())
+		btcblock1, err := s.GetBlockFromHash(block.Header.BlockHash().String())
+		Expect(err).To(BeNil())
+		Expect(btcblock1.MsgBlock().Header.Version).To(Equal(block.Header.Version))
+		Expect(btcblock1.MsgBlock().Header.MerkleRoot).To(Equal(block.Header.MerkleRoot))
+		Expect(btcblock1.MsgBlock().Header.Bits).To(Equal(block.Header.Bits))
+		Expect(btcblock1.MsgBlock().Header.Nonce).To(Equal(block.Header.Nonce))
+		// Expect(btcblock1.MsgBlock().Header.Timestamp).To(Equal(block.Header.Timestamp))
+		Expect(btcblock1.MsgBlock().Header.PrevBlock).To(Equal(block.Header.PrevBlock))
+	})
+})
 
-// var _ = Describe("GetBlockHash", func() {
+var _ = Describe("GetBlockHash", func() {
 
-// 	It("should get the blocks hash given the height", func() {
-// 		db, err := model.NewDB(sqlite.Open("gorm.db"), &gorm.Config{})
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		s := store.NewStorage(&chaincfg.RegressionNetParams, db)
-// 		block1 := &model.Block{
-// 			Hash:   "2c67b822e4a755c81dc274da32d968e815f89e084614b9613daca60ba1e8411c",
-// 			Height: 500,
-// 		}
-// 		block2 := &model.Block{
-// 			Hash:   "abcd",
-// 			Height: 1000,
-// 		}
-// 		block3 := &model.Block{
-// 			Hash:   "1234",
-// 			Height: 1500,
-// 		}
+	It("should get the blocks hash given the height", func() {
+		db, err := model.NewDB(sqlite.Open("gorm.db"), &gorm.Config{})
+		if err != nil {
+			panic(err)
+		}
+		s := store.NewStorage(&chaincfg.RegressionNetParams, db)
+		block1 := &model.Block{
+			Hash:   "2c67b822e4a755c81dc274da32d968e815f89e084614b9613daca60ba1e8411c",
+			Height: 500,
+		}
+		block2 := &model.Block{
+			Hash:   "abcd",
+			Height: 1000,
+		}
+		block3 := &model.Block{
+			Hash:   "1234",
+			Height: 1500,
+		}
 
-// 		Expect(db.Create(block1).Error).To(BeNil())
-// 		hash1, err := s.GetBlockHash(500)
-// 		Expect(err).To(BeNil())
-// 		Expect(hash1).To(Equal(block1.Hash))
+		Expect(db.Create(block1).Error).To(BeNil())
+		hash1, err := s.GetBlockHash(500)
+		Expect(err).To(BeNil())
+		Expect(hash1).To(Equal(block1.Hash))
 
-// 		Expect(db.Create(block2).Error).To(BeNil())
-// 		hash2, err := s.GetBlockHash(1000)
-// 		Expect(err).To(BeNil())
-// 		Expect(hash2).To(Equal(block2.Hash))
+		Expect(db.Create(block2).Error).To(BeNil())
+		hash2, err := s.GetBlockHash(1000)
+		Expect(err).To(BeNil())
+		Expect(hash2).To(Equal(block2.Hash))
 
-// 		Expect(db.Create(block3).Error).To(BeNil())
-// 		hash3, err := s.GetBlockHash(1500)
-// 		Expect(err).To(BeNil())
-// 		Expect(hash3).To(Equal(block3.Hash))
-// 	})
-// })
+		Expect(db.Create(block3).Error).To(BeNil())
+		hash3, err := s.GetBlockHash(1500)
+		Expect(err).To(BeNil())
+		Expect(hash3).To(Equal(block3.Hash))
+	})
+})
 
-// var _ = Describe("GetLatestBlockHash", func() {
+var _ = Describe("GetLatestBlockHash", func() {
 
-// 	It("should get the blocks hash with the max height", func() {
-// 		db, err := model.NewDB(sqlite.Open("gorm.db"), &gorm.Config{})
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		s := store.NewStorage(&chaincfg.RegressionNetParams, db)
-// 		block1 := &model.Block{
-// 			Hash:   "2c67b822e4a755c81dc274da32d968e815f89e084614b9613daca60ba1e8411c",
-// 			Height: 2000,
-// 		}
-// 		block2 := &model.Block{
-// 			Hash:   "abcd",
-// 			Height: 10000,
-// 		}
-// 		block3 := &model.Block{
-// 			Hash:   "1234",
-// 			Height: 15000,
-// 		}
-// 		Expect(db.Create(block1).Error).To(BeNil())
-// 		hash1, err := s.GetLatestBlockHash()
-// 		Expect(err).To(BeNil())
-// 		Expect(hash1).To(Equal(block1.Hash))
+	It("should get the blocks hash with the max height", func() {
+		db, err := model.NewDB(sqlite.Open("gorm.db"), &gorm.Config{})
+		if err != nil {
+			panic(err)
+		}
+		s := store.NewStorage(&chaincfg.RegressionNetParams, db)
+		block1 := &model.Block{
+			Hash:   "2c67b822e4a755c81dc274da32d968e815f89e084614b9613daca60ba1e8411c",
+			Height: 2000,
+		}
+		block2 := &model.Block{
+			Hash:   "abcd",
+			Height: 10000,
+		}
+		block3 := &model.Block{
+			Hash:   "1234",
+			Height: 15000,
+		}
+		Expect(db.Create(block1).Error).To(BeNil())
+		hash1, err := s.GetLatestBlockHash()
+		Expect(err).To(BeNil())
+		Expect(hash1).To(Equal(block1.Hash))
 
-// 		Expect(db.Create(block2).Error).To(BeNil())
-// 		hash2, err := s.GetLatestBlockHash()
-// 		Expect(err).To(BeNil())
-// 		Expect(hash2).To(Equal(block2.Hash))
+		Expect(db.Create(block2).Error).To(BeNil())
+		hash2, err := s.GetLatestBlockHash()
+		Expect(err).To(BeNil())
+		Expect(hash2).To(Equal(block2.Hash))
 
-// 		Expect(db.Create(block3).Error).To(BeNil())
-// 		hash3, err := s.GetLatestBlockHash()
-// 		Expect(err).To(BeNil())
-// 		Expect(hash3).To(Equal(block3.Hash))
-// 	})
-// })
+		Expect(db.Create(block3).Error).To(BeNil())
+		hash3, err := s.GetLatestBlockHash()
+		Expect(err).To(BeNil())
+		Expect(hash3).To(Equal(block3.Hash))
+	})
+})
 
 var _ = Describe("PutBlock", func() {
 
@@ -373,74 +373,74 @@ var _ = Describe("PutBlock", func() {
 	})
 })
 
-// var _ = Describe("CalculateLocator", func() {
-// 	It("should return an empty locator for an empty input", func() {
-// 		loc := []int{}
-// 		locator := store.CalculateLocator(loc)
-// 		Expect(locator).To(BeEmpty())
-// 	})
+var _ = Describe("CalculateLocator", func() {
+	It("should return an empty locator for an empty input", func() {
+		loc := []int{}
+		locator := store.CalculateLocator(loc)
+		Expect(locator).To(BeEmpty())
+	})
 
-// 	It("should return the same locator if the height is already 0", func() {
-// 		loc := []int{0, 0, 0}
-// 		locator := store.CalculateLocator(loc)
-// 		Expect(locator).To(Equal(loc))
-// 	})
+	It("should return the same locator if the height is already 0", func() {
+		loc := []int{0, 0, 0}
+		locator := store.CalculateLocator(loc)
+		Expect(locator).To(Equal(loc))
+	})
 
-// 	It("should calculate the locator correctly", func() {
-// 		loc := []int{10}
-// 		locator := store.CalculateLocator(loc)
-// 		Expect(locator).To(Equal([]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}))
-// 	})
+	It("should calculate the locator correctly", func() {
+		loc := []int{10}
+		locator := store.CalculateLocator(loc)
+		Expect(locator).To(Equal([]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}))
+	})
 
-// 	It("should handle large locator sizes", func() {
-// 		loc := make([]int, int(math.Pow(2, 12))+1)
-// 		locator := store.CalculateLocator(loc)
-// 		Expect(len(locator)).To(Equal(4097))
-// 		Expect(locator[12]).To(Equal(0))
-// 	})
-// })
+	It("should handle large locator sizes", func() {
+		loc := make([]int, int(math.Pow(2, 12))+1)
+		locator := store.CalculateLocator(loc)
+		Expect(len(locator)).To(Equal(4097))
+		Expect(locator[12]).To(Equal(0))
+	})
+})
 
-// var _ = Describe("GetBlockLocator", func() {
-// 	It("should return the correct block locator", func() {
+var _ = Describe("GetBlockLocator", func() {
+	It("should return the correct block locator", func() {
 
-// 		db, err := model.NewDB(sqlite.Open("gorm.db"), &gorm.Config{})
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		str := store.NewStorage(&chaincfg.RegressionNetParams, db)
-// 		h, err := str.GetLatestBlockHeight()
-// 		Expect(err).To(BeNil())
-// 		block := &model.Block{
-// 			Hash:   "2c67b822e4a755c81dc274da32d968e815f89e084614b9613daca60ba1e8411c",
-// 			Height: h,
-// 		}
-// 		//This creates a block in the database our idea is to check whether the last added block is same as the one we added
-// 		Expect(db.Create(block).Error).To(BeNil())
-// 		locator, err := str.GetBlockLocator()
-// 		Expect(err).To(BeNil())
-// 		// Expect(len(locator)).To(Equal(22))
-// 		Expect(locator[0].String()).To(Equal(block.Hash))
-// 	})
+		db, err := model.NewDB(sqlite.Open("gorm.db"), &gorm.Config{})
+		if err != nil {
+			panic(err)
+		}
+		str := store.NewStorage(&chaincfg.RegressionNetParams, db)
+		h, err := str.GetLatestBlockHeight()
+		Expect(err).To(BeNil())
+		block := &model.Block{
+			Hash:   "2c67b822e4a755c81dc274da32d968e815f89e084614b9613daca60ba1e8411c",
+			Height: h,
+		}
+		//This creates a block in the database our idea is to check whether the last added block is same as the one we added
+		Expect(db.Create(block).Error).To(BeNil())
+		locator, err := str.GetBlockLocator()
+		Expect(err).To(BeNil())
+		// Expect(len(locator)).To(Equal(22))
+		Expect(locator[0].String()).To(Equal(block.Hash))
+	})
 
-// })
+})
 
-// var _ = Describe("GetLatestBlockHeight", func() {
-// 	It("should return the correct block height", func() {
-// 		db, err := model.NewDB(sqlite.Open("gorm.db"), &gorm.Config{})
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		s := store.NewStorage(&chaincfg.RegressionNetParams, db)
-// 		height, err := s.GetLatestBlockHeight()
-// 		Expect(err).To(BeNil())
-// 		// Expect(height).To(Equal(int32(165)))
-// 		block := &model.Block{
-// 			Hash:   "2c67b822e4a755c81dc274da32d968e815f89e084614b9613daca60ba1e1111c",
-// 			Height: height + 1,
-// 		}
-// 		Expect(db.Create(block).Error).To(BeNil())
-// 		newHeight, err := s.GetLatestBlockHeight()
-// 		Expect(err).To(BeNil())
-// 		Expect(newHeight).To(Equal(height + 1))
-// 	})
-// })
+var _ = Describe("GetLatestBlockHeight", func() {
+	It("should return the correct block height", func() {
+		db, err := model.NewDB(sqlite.Open("gorm.db"), &gorm.Config{})
+		if err != nil {
+			panic(err)
+		}
+		s := store.NewStorage(&chaincfg.RegressionNetParams, db)
+		height, err := s.GetLatestBlockHeight()
+		Expect(err).To(BeNil())
+		// Expect(height).To(Equal(int32(165)))
+		block := &model.Block{
+			Hash:   "2c67b822e4a755c81dc274da32d968e815f89e084614b9613daca60ba1e1111c",
+			Height: height + 1,
+		}
+		Expect(db.Create(block).Error).To(BeNil())
+		newHeight, err := s.GetLatestBlockHeight()
+		Expect(err).To(BeNil())
+		Expect(newHeight).To(Equal(height + 1))
+	})
+})
