f176888d94129f3b4010c69a38d822c028ea35b4
jayendramadaram <dimebeatengreen8@gmail.com>
2023-09-07 14:41:13 +0530

    feat/get block from rpc

--- go.mod
+++ go.mod
@@ -3,13 +3,14 @@ module github.com/catalogfi/indexer
 go 1.18
 
 require (
-	github.com/btcsuite/btcd v0.23.0
+	github.com/btcsuite/btcd v0.23.4
 	github.com/btcsuite/btcd/btcutil v1.1.3
 	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.1
 	github.com/gin-gonic/gin v1.9.0
+	github.com/pkg/errors v0.9.1
+	github.com/zcash/lightwalletd v0.4.16
 	go.mongodb.org/mongo-driver v1.12.0
 	gorm.io/driver/postgres v1.5.0
-	gorm.io/driver/sqlite v1.5.0
 	gorm.io/gorm v1.25.1
 )
 
@@ -17,6 +18,7 @@ require (
 	github.com/btcsuite/btcd/btcec/v2 v2.1.3 // indirect
 	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f // indirect
 	github.com/btcsuite/go-socks v0.0.0-20170105172521-4720035b7bfd // indirect
+	github.com/btcsuite/websocket v0.0.0-20150119174127-31079b680792 // indirect
 	github.com/bytedance/sonic v1.8.8 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -28,6 +30,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.13.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -39,7 +42,6 @@ require (
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
-	github.com/mattn/go-sqlite3 v1.14.15 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
@@ -56,6 +58,8 @@ require (
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
+	google.golang.org/grpc v1.53.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ mongodb/zcash/peer.go
@@ -0,0 +1,146 @@
+package zcash
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+
+	"github.com/btcsuite/btcd/rpcclient"
+	"github.com/pkg/errors"
+	"github.com/zcash/lightwalletd/parser"
+	"github.com/zcash/lightwalletd/walletrpc"
+)
+
+type (
+	ZcashRpcReplyGetblock1 struct {
+		Hash  string
+		Tx    []string
+		Trees struct {
+			Sapling struct {
+				Size uint32
+			}
+			Orchard struct {
+				Size uint32
+			}
+		}
+	}
+)
+
+type Options struct {
+	RPCUser     string `json:"rpcuser"`
+	RPCPassword string `json:"rpcpassword"`
+	RPCHost     string `json:"rpchost"`
+	RPCPort     string `json:"rpcport"`
+}
+
+func getBlockFromRPC(height int) (*walletrpc.CompactBlock, error) {
+	var err error
+	var opts Options
+	var rpcClient *rpcclient.Client
+	opts = Options{
+		RPCUser:     "",
+		RPCPassword: "",
+		RPCHost:     "",
+		RPCPort:     "",
+	}
+	if opts.RPCUser != "" && opts.RPCPassword != "" && opts.RPCHost != "" && opts.RPCPort != "" {
+		connCfg := &rpcclient.ConnConfig{
+			Host:         net.JoinHostPort(opts.RPCHost, opts.RPCPort),
+			User:         opts.RPCUser,
+			Pass:         opts.RPCPassword,
+			HTTPPostMode: true, // Zcash only supports HTTP POST mode
+			DisableTLS:   true, // Zcash does not provide TLS by default
+		}
+		rpcClient, err = rpcclient.New(connCfg, nil)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		panic("rpcClient is nil")
+	}
+
+	// `block.ParseFromSlice` correctly parses blocks containing v5
+	// transactions, but incorrectly computes the IDs of the v5 transactions.
+	// We temporarily paper over this bug by fetching the correct txids via a
+	// verbose getblock RPC call, which returns the txids.
+	//
+	// Unfortunately, this RPC doesn't return the raw hex for the block,
+	// so a second getblock RPC (non-verbose) is needed (below).
+
+	heightJSON, err := json.Marshal(strconv.Itoa(height))
+	if err != nil {
+		panic(fmt.Errorf("getBlockFromRPC: %v at height %v", err, height))
+	}
+	params := make([]json.RawMessage, 2)
+	params[0] = heightJSON
+	// Fetch the block using the verbose option ("1") because it provides
+	// both the list of txids, which we're not yet able to compute for
+	// Orchard (V5) transactions, and the block hash (block ID), which
+	// we need to fetch the raw data format of the same block. Don't fetch
+	// by height in case a reorg occurs between the two getblock calls;
+	// using block hash ensures that we're fetching the same block.
+	params[1] = json.RawMessage("1")
+	result, rpcErr := rpcClient.RawRequest("getblock", params)
+	if rpcErr != nil {
+		// Check to see if we are requesting a height the zcashd doesn't have yet
+		if (strings.Split(rpcErr.Error(), ":"))[0] == "-8" {
+			return nil, nil
+		}
+		return nil, errors.Wrap(rpcErr, "error requesting verbose block")
+	}
+	var block1 ZcashRpcReplyGetblock1
+	err = json.Unmarshal(result, &block1)
+	if err != nil {
+		return nil, err
+	}
+	blockHash, err := json.Marshal(block1.Hash)
+	if err != nil {
+		fmt.Errorf("getBlockFromRPC: %v at height %v", err, height)
+	}
+	params[0] = blockHash
+	params[1] = json.RawMessage("0") // non-verbose (raw hex)
+	result, rpcErr = rpcClient.RawRequest("getblock", params)
+
+	// For some reason, the error responses are not JSON
+	if rpcErr != nil {
+		return nil, errors.Wrap(rpcErr, "error requesting block")
+	}
+
+	var blockDataHex string
+	err = json.Unmarshal(result, &blockDataHex)
+	if err != nil {
+		return nil, errors.Wrap(err, "error reading JSON response")
+	}
+
+	blockData, err := hex.DecodeString(blockDataHex)
+	if err != nil {
+		return nil, errors.Wrap(err, "error decoding getblock output")
+	}
+
+	block := parser.NewBlock()
+	rest, err := block.ParseFromSlice(blockData)
+	if err != nil {
+		return nil, errors.Wrap(err, "error parsing block")
+	}
+	if len(rest) != 0 {
+		return nil, errors.New("received overlong message")
+	}
+	if block.GetHeight() != height {
+		return nil, errors.New("received unexpected height block")
+	}
+	for i, t := range block.Transactions() {
+		txid, err := hex.DecodeString(block1.Tx[i])
+		if err != nil {
+			return nil, errors.Wrap(err, "error decoding getblock txid")
+		}
+		// convert from big-endian
+		t.SetTxID(parser.Reverse(txid))
+	}
+	r := block.ToCompact()
+	r.ChainMetadata.SaplingCommitmentTreeSize = block1.Trees.Sapling.Size
+	r.ChainMetadata.OrchardCommitmentTreeSize = block1.Trees.Orchard.Size
+	return r, nil
+}
